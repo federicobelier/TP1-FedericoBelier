e19cbcfd8136fe962e813f4418e71fbed4dd66a2
FedericoB <federico.codedev@gmail.com>
2022-08-31 09:51:20 -0400

    Calculadora de Calorias start

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Calculadora de Calorias
+
+//Variables
+
+let genero = prompt ("Que genero eres: Hombre o Mujer?")
+let peso = prompt ('Cuanto pesas? (En kilogramos)')
+let altura = prompt ('Caul es tu estatura?')
+let edad = prompt ('Cuantos años tienes?')
+let actividad = prompt ('Caunto ejercitas?')
+
+console.log (genero, peso, altura, edad)
+
+
+
+    Mujer
+    ((655+(9,6*${peso})+(1,8*altura)-(4,7*edad))*)
